5f8fabe5fe5431db0a843e3f018ee685d42e45cb
Robert Pyke <robert.j.pyke@gmail.com>
2012-02-28 18:14:56 +1000

    Fixed bad species test fixture record generation.

--- app/Test/Fixture/SpeciesFixture.php
+++ app/Test/Fixture/SpeciesFixture.php
@@ -8,7 +8,7 @@ class SpeciesFixture extends CakeTestFixture {
 
 	// Setup some dummy records
 	public $records = array(
-		array('id' => 1, 'name' => 'emu', 'created' => '2007-03-18 10:39:23', 'updated' => '2007-03-18 10:41:31'),
-		array('id' => 2, 'name' => 'crow', 'created' => '2007-03-19 10:39:23', 'updated' => '2007-03-20 10:41:31')
+		array('id' => 1, 'name' => 'emu', 'created' => '2007-03-18 10:39:23', 'modified' => '2007-03-18 10:41:31'),
+		array('id' => 2, 'name' => 'crow', 'created' => '2007-03-19 10:39:23', 'modified' => '2007-03-20 10:41:31')
 	);
 }
