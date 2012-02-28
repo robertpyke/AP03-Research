8cc6ee8e016723dd0a8c442e911bd0be8d5b16ec
Robert Pyke <robert.j.pyke@gmail.com>
2012-02-28 14:47:07 +1000

    Added some initial validation to the Species Model.

--- app/Model/Species.php
+++ app/Model/Species.php
@@ -1,4 +1,15 @@
 <?
 class Species extends AppModel {
 	public $name = 'Species';
+	// A Species has many occurences.
+	public $hasMany = 'Occurrence';
+
+	public $validate = array(
+		'name' => array(
+			'rule' => 'alphaNumeric',
+			'required' => true,                           // A name must be specified
+			'allowEmpty' => false,                        // You can't specify an empty string
+			'message' => 'Alphabets and numbers only',
+		)
+	);
 }
